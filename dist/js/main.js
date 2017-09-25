c1ca9f2ed2c9dca222c97e8ad6a62a584d2154c4
frank <frank.thomsen@gmail.com>
2017-09-25 15:46:23 +0200

    cleanup and enable distance calculation

--- dist/js/main.js
+++ dist/js/main.js
@@ -20,15 +20,13 @@ var geolocator = (function() {
         geo_map_polyline,
         geo_map_icon_a = L.icon({
             iconUrl: 'img/icon-a.svg',
-            iconSize: [64, 64],
-            iconAnchor: [32, 64],
-            popupAnchor: [32, 16]
+            iconSize: [48, 48],
+            iconAnchor: [24, 48]
         }),
         geo_map_icon_b = L.icon({
             iconUrl: 'img/icon-b.svg',
-            iconSize: [64, 64],
-            iconAnchor: [32, 64],
-            popupAnchor: [32, 16]
+            iconSize: [48, 48],
+            iconAnchor: [24, 48]
         }),
         geo_online = navigator.onLine,
         geo_routes = [],
@@ -111,16 +109,7 @@ var geolocator = (function() {
             'm'
         ].join(''));
         geo_current_position = position;
-        ui_pos.innerHTML = `
-            <dl>
-                <dt>Latitude</dt>  
-                <dd>${position.coords.latitude.toFixed(6)}</dd>
-                <dt>Longitude</dt>  
-                <dd>${position.coords.longitude.toFixed(6)}</dd>
-                <dt>Position accuracy</dt>  
-                <dd>${position.coords.accuracy.toFixed(0)}m</dd>
-            </dl>
-        `; 
+        redrawPosInfo(geo_current_position);
         stopSpinner();
         updateMarker([position.coords.latitude, position.coords.longitude]);
     }
@@ -164,15 +153,42 @@ var geolocator = (function() {
         geo_route.track.coords.push(latlon);
         ui_log_list.innerHTML += '<li style="font-size: smaller">Waypoint set at ' + latlon + '</li>';
         geo_map_track_marker = L.marker(latlon, {icon:  geo_map_icon_b}).addTo(geo_map);
-        geo_route.redraw(); 
+        calcDistance(geo_route.track.coords);
+        redrawPosInfo(geo_current_position);
+        geo_route.redraw();
     }
 
     function redrawMap() {
         if (geo_map_polyline !== undefined ) {
             geo_map.removeLayer(geo_map_polyline);
         }
-        geo_map_polyline = L.polyline(geo_route.track.coords, {color: '#aabbff'}).addTo(geo_map);
+        geo_map_polyline = L.polyline(geo_route.track.coords, {color: '#401B00'}).addTo(geo_map);
         geo_map.fitBounds(geo_map_polyline.getBounds());
+    }
+
+    function redrawPosInfo(position) {
+        ui_pos.innerHTML = `
+            <dl class="location-info">
+                <dt>Latitude</dt>  
+                <dd>${position.coords.latitude.toFixed(6)}</dd>
+                <dt>Longitude</dt>  
+                <dd>${position.coords.longitude.toFixed(6)}</dd>
+                <dt>Position accuracy</dt>  
+                <dd>${position.coords.accuracy.toFixed(0)}m</dd>
+                <dt>Altitude</dt>
+                <dd>${position.coords.altitude ? position.coords.altitude.toFixed(6) : '-'}</dd>
+                <dt>Altitude accuracy</dt>
+                <dd>${position.coords.altitudeAccuracy ? position.coords.altitudeAccuracy.toFixed(6) : '-'}</dd>
+                <dt>Heading</dt>
+                <dd>${position.coords.heading ? position.coords.heading.toFixed(0) : '-'}</dd>
+                <dt>Speed</dt>
+                <dd>${position.coords.speed ? position.coords.speed.toFixed(0) + 'm/s' : '-'}</dd>
+                <dt>Latest position time</dt>
+                <dd>${new Date().toLocaleTimeString()}</dd>
+                <dt>Distance travelled</dt>
+                <dd>${(geo_route.track.distance / 1000).toFixed(0) + 'km'}</dd>
+            </dl>
+        `;
     }
 
     function listRoutes() {
@@ -188,6 +204,14 @@ var geolocator = (function() {
         ui_log_list.innerHTML += '<li style="font-size: smaller">' + data + '</li>';
     }
 
+    function calcDistance(latlngs) {
+        if (latlngs.length > 1) {
+            for (var l = 1; l < latlngs.length; l++) {
+                geo_route.track.distance += geo_map.distance(latlngs[l], latlngs[l-1]);
+            }
+        }
+    }
+
     function getSingleLocation() {
         startSpinner()
         if ('geolocation' in navigator) {
@@ -196,7 +220,7 @@ var geolocator = (function() {
                     logData('Got current position: ' + position.coords.latitude.toFixed(4) + ',' + position.coords.longitude.toFixed(4) + '; acc: ' + position.coords.accuracy + 'm');
                     geo_current_position = position;
                     ui_pos.innerHTML = `
-                        <dl>
+                        <dl class="location-info">
                             <dt>Latitude</dt>  
                             <dd>${position.coords.latitude.toFixed(4)}</dd>
                             <dt>Longitude</dt>  
@@ -229,102 +253,7 @@ geolocator.init();
 
 
 /*
-function onLocationFound(e) {
-        // Get prior coord to compare
-        var pre_coord = geo_route.coords[geo_route.coords.length - 1];
-        var dist;
-        if (pre_coord) {
-            dist = geo_map.distance( [e.latitude, e.longitude], [pre_coord.latitude, pre_coord.longitude] );
-            console.log('distance');
-            console.log(geo_map.distance( [e.latitude, e.longitude], [pre_coord.latitude, pre_coord.longitude] ));
-        } else {
-            dist = 0;
-        }
-        // Push position to current route
-        geo_route.coords.push({
-            latitude: e.latitude,
-            longitude: e.longitude,
-            distance: dist,
-            accuracy: e.accuracy
-        });
-        redrawMap(geo_route);
-        redrawLog(geo_route);
-        redrawLocationInfo(geo_route);
-    }
-    
-    function onLocationError(e) {
-        alert(e.message);
-    }
-    
-    function redrawMap(route) {
-        // Clean up map
-        if (geo_map_marker !== undefined) {
-            geo_map_marker.remove();    
-        }
-        if (geo_map_circle !== undefined) {
-            geo_map_circle.remove();
-        }
-        if (geo_map_polyline !== undefined) {
-            geo_map_polyline.remove();    
-        }
-        // Get positions for new map markers and calculate running distance
-        route.distance = 0;
-        var latlngs = [];
-        for (var c in route.coords) {
-            latlngs.push([ route.coords[c].latitude, route.coords[c].longitude ]);
-            // Now we're at it, add the distances together
-            route.distance += route.coords[c].distance;
-        }
-        // Draw the route
-        geo_map_polyline = L.polyline(latlngs, {color: 'blue'}).addTo(geo_map);
-        // zoom the map to the polyline
-        geo_map.fitBounds(geo_map_polyline.getBounds());
-        // Redraw the current position
-        var radius = route.coords[route.coords.length - 1].accuracy / 2;
-        var latlng = [ route.coords[route.coords.length - 1].latitude, route.coords[route.coords.length - 1].longitude ];
-        geo_map_marker = L.marker(latlng).addTo(geo_map).bindPopup("You are within " + radius + " meters from this point");
-        geo_map_circle = L.circle(latlng, radius).addTo(geo_map);
-    }
-    
-    function redrawLog(route) {
-        logdisplay.innerHTML = "<strong>" + route.name + "</strong>";
-        for (var r in route.coords) {
-            logdisplay.innerHTML += "<p>latlong: " + route.coords[r].latitude + "," + route.coords[r].longitude + "; distance: " + route.coords[r].distance + "</p>";    
-        }
-    }
-    
-    function redrawLocationInfo(route) {
-        // Display geolocation data on screen
-        console.log(route);
-        var coord = route.coords[route.coords.length - 1];
-        locdisplay.innerHTML = "<dl>"
-                    + "<dt>Route</dt><dd>" + route.name + "</dd>"
-                    + "<dt>Latitude</dt><dd>" + coord.latitude.toFixed(4) + "</dd>"
-                    + "<dt>Longitude</dt><dd>" + coord.longitude.toFixed(4) + "</dd>"
-                    + "<dt>Distance</dt><dd>" + route.distance.toFixed(0) + "m</dd>"
-                    + "</dl>";
-    }
-    
-    function startTracking() {
-        geo_route = {
-            name: 'New route ' + geo_routes.length,
-            coords: [],
-            distance: 0
-        };
-        geo_map.locate({
-            setView: true,
-            enableHighAccuracy: true,
-            watch: true
-        });
-    }
-    
-    function stopTracking() {
-        geo_map.stopLocate();
-        geo_routes.push(geo_route);
-        saveRoutes(geo_routes);
-        listRoutes(geo_routes);
-    }
-    
+        
     function saveRoutes(routes) {
         localStorage.setItem("geolocatorSavedRoutes", JSON.stringify(routes));
     }
