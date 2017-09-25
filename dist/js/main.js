3fb94a5742b01ed401dba2bd6e05fbcfa409b76f
frank <frank.thomsen@gmail.com>
2017-09-25 08:46:16 +0200

    added improved service worker to phase out cache.manifest

--- dist/js/main.js
+++ dist/js/main.js
@@ -219,27 +219,155 @@ geolocator.init();
 */
 
 var service_worker = (function() {
-    
     var sw = {};
-    
     sw.init = init;
-    
     function init() {
+        
         if ('serviceWorker' in navigator) {
-            window.addEventListener('load', function() {
-                navigator.serviceWorker.register('sw.js').then(function(registration) {
-                // Registration was successful
-                console.log('ServiceWorker registration successful with scope: ', registration.scope);
-                }).catch(function(err) {
-                // registration failed :(
-                console.log('ServiceWorker registration failed: ', err);
+            console.log('CLIENT: service worker registration in progress.');
+            
+            navigator.serviceWorker.register('sw.js')
+                .then(function(registration) {
+                    // Registration was successful
+                    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+                })
+                .catch(function(err) {
+                    // registration failed :(
+                    console.log('ServiceWorker registration failed: ', err);
                 });
-            });
+        
+        } else {
+            console.log('CLIENT: service worker is not supported.');
         }
+
     }
-    
     return sw;
-    
 }());
 
 service_worker.init();
+
+
+/*
+function onLocationFound(e) {
+        // Get prior coord to compare
+        var pre_coord = geo_route.coords[geo_route.coords.length - 1];
+        var dist;
+        if (pre_coord) {
+            dist = geo_map.distance( [e.latitude, e.longitude], [pre_coord.latitude, pre_coord.longitude] );
+            console.log('distance');
+            console.log(geo_map.distance( [e.latitude, e.longitude], [pre_coord.latitude, pre_coord.longitude] ));
+        } else {
+            dist = 0;
+        }
+        // Push position to current route
+        geo_route.coords.push({
+            latitude: e.latitude,
+            longitude: e.longitude,
+            distance: dist,
+            accuracy: e.accuracy
+        });
+        redrawMap(geo_route);
+        redrawLog(geo_route);
+        redrawLocationInfo(geo_route);
+    }
+    
+    function onLocationError(e) {
+        alert(e.message);
+    }
+    
+    function redrawMap(route) {
+        // Clean up map
+        if (geo_map_marker !== undefined) {
+            geo_map_marker.remove();    
+        }
+        if (geo_map_circle !== undefined) {
+            geo_map_circle.remove();
+        }
+        if (geo_map_polyline !== undefined) {
+            geo_map_polyline.remove();    
+        }
+        // Get positions for new map markers and calculate running distance
+        route.distance = 0;
+        var latlngs = [];
+        for (var c in route.coords) {
+            latlngs.push([ route.coords[c].latitude, route.coords[c].longitude ]);
+            // Now we're at it, add the distances together
+            route.distance += route.coords[c].distance;
+        }
+        // Draw the route
+        geo_map_polyline = L.polyline(latlngs, {color: 'blue'}).addTo(geo_map);
+        // zoom the map to the polyline
+        geo_map.fitBounds(geo_map_polyline.getBounds());
+        // Redraw the current position
+        var radius = route.coords[route.coords.length - 1].accuracy / 2;
+        var latlng = [ route.coords[route.coords.length - 1].latitude, route.coords[route.coords.length - 1].longitude ];
+        geo_map_marker = L.marker(latlng).addTo(geo_map).bindPopup("You are within " + radius + " meters from this point");
+        geo_map_circle = L.circle(latlng, radius).addTo(geo_map);
+    }
+    
+    function redrawLog(route) {
+        logdisplay.innerHTML = "<strong>" + route.name + "</strong>";
+        for (var r in route.coords) {
+            logdisplay.innerHTML += "<p>latlong: " + route.coords[r].latitude + "," + route.coords[r].longitude + "; distance: " + route.coords[r].distance + "</p>";    
+        }
+    }
+    
+    function redrawLocationInfo(route) {
+        // Display geolocation data on screen
+        console.log(route);
+        var coord = route.coords[route.coords.length - 1];
+        locdisplay.innerHTML = "<dl>"
+                    + "<dt>Route</dt><dd>" + route.name + "</dd>"
+                    + "<dt>Latitude</dt><dd>" + coord.latitude.toFixed(4) + "</dd>"
+                    + "<dt>Longitude</dt><dd>" + coord.longitude.toFixed(4) + "</dd>"
+                    + "<dt>Distance</dt><dd>" + route.distance.toFixed(0) + "m</dd>"
+                    + "</dl>";
+    }
+    
+    function startTracking() {
+        geo_route = {
+            name: 'New route ' + geo_routes.length,
+            coords: [],
+            distance: 0
+        };
+        geo_map.locate({
+            setView: true,
+            enableHighAccuracy: true,
+            watch: true
+        });
+    }
+    
+    function stopTracking() {
+        geo_map.stopLocate();
+        geo_routes.push(geo_route);
+        saveRoutes(geo_routes);
+        listRoutes(geo_routes);
+    }
+    
+    function saveRoutes(routes) {
+        localStorage.setItem("geolocatorSavedRoutes", JSON.stringify(routes));
+    }
+    
+    function listRoutes(routes) {
+        routedisplay.innerHTML = '';
+        for (var r in routes) {
+            routedisplay.innerHTML += '<li><button class="btn" onclick="geolocator.loadRoute(' + r + ')">' + routes[r].name + '</button><button class="btn" onclick="geolocator.deleteRoute(' + r + ')">Delete</button></li>';
+        }
+    }
+    
+    function loadRoute(route_id) {
+        location.hash = '';
+        geo_route = geo_routes[route_id];
+        redrawLog(geo_route);
+        redrawLocationInfo(geo_route, geo_route.coords[geo_route.coords.length - 1]);
+        redrawMap(geo_route);
+    }
+    
+    function deleteRoute(route_id) {
+        if (confirm('Delete?')) {
+            geo_routes.splice(route_id, 1);
+            saveRoutes(geo_routes);
+            listRoutes(geo_routes);    
+        }
+    }
+*/
