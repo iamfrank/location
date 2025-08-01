bd2607000e1a46dca9363e01c028fda60a7dabae
Frank R Thomsen <frank@septima.dk>
2025-08-01 16:28:17 +0200

    add accuracy cirle

--- js/map.js
+++ js/map.js
@@ -1,56 +1,67 @@
-import * as L from './leaflet/leaflet-src.esm.js'
+import * as L from "./leaflet/leaflet-src.esm.js";
 
 // Define LeafletMap component
 export class LeafletMap extends HTMLElement {
-
   set setLocation(location) {
-    if (this.current_marker) {
-      this.current_marker.remove()
+    if (this.current_marker && this.circle_marker) {
+      this.current_marker.remove();
+      this.circle_marker.remove();
     }
-    this.current_marker = L.marker([location.latitude, location.longitude], { icon: this.icon_a }).addTo(this.ui_map)
-    this.current_marker._icon.location_data = location
-    this.ui_map.flyTo([location.latitude, location.longitude], 14)
+    this.current_marker = L.marker([location.latitude, location.longitude], {
+      icon: this.icon_a,
+    }).addTo(this.ui_map);
+    this.circle_marker = L.circle([location.latitude, location.longitude], {
+      radius: location.accuracy,
+    }).addTo(this.ui_map);
+    this.current_marker._icon.location_data = location;
+    this.ui_map.flyTo([location.latitude, location.longitude], 14);
   }
 
   set setMarkers(locations) {
-    this.saved_markers.clearLayers()
+    this.saved_markers.clearLayers();
     for (let p in locations) {
-      let pos = locations[p]
-      const marker = L.marker([pos.latitude, pos.longitude], { icon: this.icon_b })
-      this.saved_markers.addLayer(marker)
-      marker._icon.location_data = pos
+      let pos = locations[p];
+      const marker = L.marker([pos.latitude, pos.longitude], {
+        icon: this.icon_b,
+      });
+      this.saved_markers.addLayer(marker);
+      marker._icon.location_data = pos;
     }
   }
 
   constructor() {
-    super()
+    super();
 
     // Create markup and attach to the DOM
-    this.map = document.createElement('div')
-    this.map.setAttribute('id', 'lftmap')
-    this.appendChild(this.map)
+    this.map = document.createElement("div");
+    this.map.setAttribute("id", "lftmap");
+    this.appendChild(this.map);
 
-    this.ui_map = L.map('lftmap', {zoomControl: false}).setView([55, 11.5], 6)
-    L.control.scale({imperial: false}).addTo(this.ui_map)
+    this.ui_map = L.map("lftmap", { zoomControl: false }).setView(
+      [55, 11.5],
+      6,
+    );
+    L.control.scale({ imperial: false }).addTo(this.ui_map);
     this.icon_a = L.icon({
-      iconUrl: './img/marker-red.svg',
+      iconUrl: "./img/marker-red.svg",
       iconSize: [30, 45],
       iconAnchor: [15, 45],
-      popupAnchor: [0, -30]
-    })
+      popupAnchor: [0, -30],
+    });
     this.icon_b = L.icon({
-      iconUrl: './img/marker-black.svg',
+      iconUrl: "./img/marker-black.svg",
       iconSize: [30, 45],
       iconAnchor: [15, 45],
-      popupAnchor: [0, -30]
-    })
-    this.current_marker = null
-    this.saved_markers = L.layerGroup().addTo(this.ui_map)
+      popupAnchor: [0, -30],
+    });
+    this.current_marker = null;
+    this.circle_marker = null;
+    this.saved_markers = L.layerGroup().addTo(this.ui_map);
 
     // Initialize Leaflet
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(this.ui_map)
+    L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+    }).addTo(this.ui_map);
   }
-
 }
