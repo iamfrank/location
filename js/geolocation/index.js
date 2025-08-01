68f5d98af402fd761a899a1ef71344c931428f58
Frank R Thomsen <frank@septima.dk>
2025-08-01 15:12:41 +0200

    use new location api

---
+++ js/geolocation/index.js
@@ -0,0 +1,171 @@
+export default class GeoLoc {
+  options = {
+    enableHighAccuracy: true,
+    timeout: 60000,
+    maximumAge: 5000,
+    accuracyThreshold: 10,
+  };
+  geolocationResult = null;
+  geolocationResultCache = [];
+  status = null;
+  trackerId = null;
+
+  constructor(options) {
+    if (!navigator.geolocation) {
+      this.status = "Geolocation is not supported by your browser";
+      console.error(this.status);
+    } else if (options) {
+      this.options = Object.assign({}, options);
+    }
+  }
+
+  get coordinate() {
+    return [
+      this.geolocationResult.coords.longitude,
+      this.geolocationResult.coords.latitude,
+    ];
+  }
+  get accuracy() {
+    return this.geolocationResult.coords.accuracy;
+  }
+  get time() {
+    return this.geolocationResult.timestamp;
+  }
+  get status() {
+    return this.status;
+  }
+  get crs() {
+    return "WGS84";
+  }
+  get isAccurate() {
+    if (
+      this.geolocationResult.coords.accuracy < this.options.accuracyThreshold
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  getPosition() {
+    return new Promise((resolve, reject) => {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          if (position instanceof GeolocationPosition) {
+            this.status = "Ready";
+            this.geolocationResult = position;
+            this.dispatchPosition(position);
+            resolve(position);
+          }
+        },
+        (posError) => {
+          if (posError instanceof GeolocationPositionError) {
+            this.status = "Error";
+            this.geolocationResult = null;
+            reject(posError);
+          } else {
+            reject("Geolocation API returned an unknown error");
+          }
+        },
+        this.options,
+      );
+    });
+  }
+
+  dispatchPosition(position) {
+    document.body.dispatchEvent(
+      new CustomEvent("change:geolocation", {
+        detail: position,
+        bubbles: true,
+        composed: true,
+      }),
+    );
+  }
+
+  trackPosition() {
+    this.geolocationResultCache = [];
+    this.trackerId = navigator.geolocation.watchPosition(
+      (position) => {
+        if (position instanceof GeolocationPosition) {
+          this.status = "Tracking";
+          this.geolocationResultCache.push(position);
+          if (this.geolocationResultCache.length > 10) {
+            this.geolocationResultCache.shift();
+          }
+          this.geolocationResult = position;
+          this.dispatchPosition(position);
+        }
+      },
+      (posError) => {
+        if (posError instanceof GeolocationPositionError) {
+          this.status = "Error";
+          this.geolocationResult = null;
+          this.trackEnd();
+          console.log(posError);
+        } else {
+          console.log("Geolocation API returned an unknown error");
+        }
+      },
+      this.options,
+    );
+  }
+
+  trackEnd() {
+    navigator.geolocation.clearWatch(this.trackerId);
+  }
+
+  positionErrorHandler(posError) {
+    if (posError instanceof GeolocationPositionError) {
+      this.status = "Error";
+      this.geolocationResult = null;
+      reject(posError);
+    } else {
+      reject("Geolocation API returned an unknown error");
+    }
+  }
+
+  renderHTML() {
+    const posData = this.geolocationResult.coords;
+    const element = document.createElement("dl");
+    let htmlContent = "<dl>";
+    htmlContent += this.renderDefinition(
+      "longitude",
+      posData.longitude,
+      "decimal degrees",
+    );
+    htmlContent += this.renderDefinition(
+      "latitude",
+      posData.latitude,
+      "decimal degrees",
+    );
+    htmlContent += this.renderDefinition("accuracy", posData.accuracy, "m");
+    htmlContent += this.renderDefinition(
+      "Is that very accurate?",
+      this.isAccurate,
+    );
+    htmlContent += this.renderDefinition("altitude", posData.altitude, "m");
+    htmlContent += this.renderDefinition(
+      "altitudeAccuracy",
+      posData.altitudeAccuracy,
+      "m",
+    );
+    htmlContent += this.renderDefinition("heading", posData.heading, "degrees");
+    htmlContent += this.renderDefinition("speed", posData.speed, "m/s");
+    htmlContent += this.renderDefinition(
+      "Timestamp",
+      new Date(this.geolocationResult.timestamp).toLocaleString(),
+    );
+
+    htmlContent += "</dl>";
+    element.innerHTML = htmlContent;
+    return htmlContent;
+  }
+
+  renderDefinition(title, data, unit = "") {
+    if (typeof title === "string") {
+      return `<dt>${title}${unit !== "" ? ` (${unit})` : ""}</dt><dd>${data}</dd>`;
+    } else {
+      return "";
+    }
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 // Import modules
-import { LeafletMap } from './map.js'
-import { LocationInfo } from './location-info.js'
-import { getLocations } from './state.js'
-import { fetchCurrentPosition } from './position.js'
-import { LocationList } from './location-list.js'
-import { LocationMessage } from './messages.js'
+import { LeafletMap } from "./map.js";
+import GeoLoc from "./geolocation/index.js";
+import { LocationInfo } from "./location-info.js";
+import { getLocations } from "./state.js";
+import { fetchCurrentPosition } from "./position.js";
+import { LocationList } from "./location-list.js";
+import { LocationMessage } from "./messages.js";
 
 // Init web components
-customElements.define('location-info', LocationInfo)
-customElements.define('leaflet-map', LeafletMap)
-customElements.define('location-list', LocationList)
-customElements.define('location-message', LocationMessage)
+customElements.define("location-info", LocationInfo);
+customElements.define("leaflet-map", LeafletMap);
+customElements.define("location-list", LocationList);
+customElements.define("location-message", LocationMessage);
 
 // Init state and elements
-const map_el = document.getElementById('lflt')
-fetchCurrentPosition()
+const map_el = document.getElementById("lflt");
+const geol = new GeoLoc();
 
 /*
 // Register service worker for offline use
@@ -34,36 +35,42 @@ if ('serviceWorker' in navigator) {
 */
 
 // Show saved locations in map on page load
-window.addEventListener('load', function () {
-  map_el.setMarkers = getLocations()
-})
+window.addEventListener("load", function () {
+  map_el.setMarkers = getLocations();
+});
 
 // On new location event, update map and action panel
-document.addEventListener('updateposition', function (ev) {
-  map_el.setLocation = ev.detail.position()
-})
+document.addEventListener("change:geolocation", function (ev) {
+  map_el.setLocation = ev.detail.coords;
+});
 
 // When locations are changed, update map markers
-document.addEventListener('updatelocations', function (ev) {
-  map_el.setMarkers = getLocations()
-})
+document.addEventListener("updatelocations", function (ev) {
+  map_el.setMarkers = getLocations();
+});
 
 // Handle clicks and touches
-document.addEventListener('click', function (ev) {
-  if (ev.target.classList.contains('leaflet-marker-icon')) {
-    const locationInfoElement = document.createElement('location-info')
-    locationInfoElement.setLocation(ev.target.location_data)
-    document.body.append(locationInfoElement)
+document.addEventListener("click", function (ev) {
+  if (ev.target.classList.contains("leaflet-marker-icon")) {
+    const locationInfoElement = document.createElement("location-info");
+    locationInfoElement.setLocation(ev.target.location_data);
+    document.body.append(locationInfoElement);
   }
-})
+});
 
 // Opens the location list
-document.querySelector('.location-list-toggle').addEventListener('click', () => {
-  const locationListElement = document.createElement('location-list')
-  document.body.append(locationListElement)
-})
+document
+  .querySelector(".location-list-toggle")
+  .addEventListener("click", () => {
+    const locationListElement = document.createElement("location-list");
+    document.body.append(locationListElement);
+  });
 
 // Triggers relocating position
-document.querySelector('.location-update').addEventListener('click', () => {
-  fetchCurrentPosition()
-})
+document
+  .querySelector(".location-update")
+  .addEventListener("click", async () => {
+    await geol.getPosition();
+  });
+
+geol.getPosition();
