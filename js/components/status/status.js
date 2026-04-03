49bb37094c2dfcd37102ad3a8c805247032d66ef
Frank R Thomsen <frank@septima.dk>
2026-04-03 16:16:32 +0200

    add info to screen

--- js/components/status/status.js
+++ js/components/status/status.js
@@ -17,9 +17,9 @@ export class StatusBar extends HTMLElement {
     const navigation = get("navigate");
     const current = get("current");
     this.innerHTML = `
-      ${current ? `<dl><dt>pos</dt><dd>${formatCoords(current.latitude, current.longitude)}</dd></dl>` : ""}
-      ${navigation?.from ? `<dl><dt>orig</dt><dd>${formatCoords(navigation.from.latitude, navigation.from.longitude)}</dd></dl>` : ""}
-      ${navigation?.to ? `<dl><dt>target</dt><dd>${formatCoords(navigation.to.latitude, navigation.to.longitude)}</dd></dl>` : ""}
+      ${current ? `<dl><dt>pos</dt><dd>${current.title} ${formatCoords(current.latitude, current.longitude)}</dd></dl>` : ""}
+      ${navigation?.from ? `<dl><dt>orig</dt><dd>${navigation.from.title} ${formatCoords(navigation.from.latitude, navigation.from.longitude)}</dd></dl>` : ""}
+      ${navigation?.to ? `<dl><dt>target</dt><dd>${navigation.to.title} ${formatCoords(navigation.to.latitude, navigation.to.longitude)}</dd></dl>` : ""}
       ${navigation?.from && navigation?.to ? this.renderDistanceAndHeading(navigation.from, navigation.to) : ""}
     `;
   }
